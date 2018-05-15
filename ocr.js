b467601a1698355684620be529737139ae91ba44
qc-an <1063819472@qq.com>
2018-05-15 09:57:56 +0800

    ocr.js

--- ocr.js
+++ ocr.js
@@ -18,4 +18,118 @@ var ocrDemo = {
     },
     
     resetCanvas: function() {
-        var canvas = document.
+        var canvas = document.getElementById('canvas');
+        car ctx = canvas.getContext('2d');
+        this.data = [];
+        ctx.fillStyle = this.BLACK;
+        ctx.fillRect(0,0, this.CANVAS_WIDTH, this.CANVAS_WIDTH);
+        var matrixSize = 400;
+        while(matrixSize--) this.data.push(0);
+        this.drawGrid(ctx);
+        //things happening and reaction
+        canvas.onmousemove = function(e) { this.onMouseMove(e, ctx, canvas)}.bind(this);
+        canvas.onmousedown = function(e) { this.onMouseDown(e, ctx, canvas)}.bind(this);
+        canvas.onmouseup = function(e) {this.conMouseUp(e, ctx, canvas)}.bin(this);
+    },
+
+    drawGrid: function(ctx){
+        for (var x = this.PIXEL_WIDTH, y = this.PIXEL_WIDTH; x <this.CANVAS_WIDTH; x+= this.PIXEL_WIDTH, y+= this.PIXEL_WIDTH){
+            ctx.strokeStyle = this.BLUE;
+            ctx.beginPath();
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, this.CANVAS_WIDTH);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(0, y);
+            ctx.lineTo(this.CANVAS_WIDTH, y);
+            ctx.stroke();
+          }
+      },
+
+    onMouseMove: function(e, ctx, canvas){
+        if (!canvas.isDrawing){
+            return;
+        }
+        this.fillSquare(ctx, e.clientX = canvas.offsetLeft, e.clientY - canvas.offsetTop);
+     },
+
+    onMouseDown: function(e, ctx, canvas){
+        canvas.isDrawing = true;
+        this.fillSquare(ctx, e.clientY - canvas.offsetTop);
+    },
+
+    onMouseUp: function(e) {
+        canvas.isDrawing = false;
+    },
+
+    fillSquare: function(ctx, x,y){
+        var xPixel = Math.floor(x / this.PIXEL_WIDTH);
+        var yPixel = Math.floor(y / this.PIXEL_WIDTH);
+        this.data[((xPixel - 1) * this.TRANSLATED_WIDTH + yPixel) - 1] = 1;
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(xPixel * this.PIXEL_WIDTH, yPixel * this.PIXEL_WIDTH, this.PIXEL_WIDTH, this.PIXEL_WIDTH);
+    },
+
+    train: function(){
+        var digitCal = document.getElementById("digit").value;
+        if (!digit || this.data.indexOf(1) < 0 )
+    {
+            alert("please type and draw a digit value in order to train the network");
+            return;
+        }
+         this.trainArray.push({"y0": this.data, "label": parseInt(digitVal)});
+         this.trainRequestCount++;
+
+         if (this.trainingRequestCount == this.BATCH_SIZE){
+             alert("Sending training data to server...");
+             var json = {
+                 trainArray: this.trainArray,
+                 train:true
+             };
+             this.sendData(json);
+             this.trainingRequestCount = 0;
+             this.trainArray = [];
+            }
+       },
+
+    test: function(){
+        if (this.data.indexOf(1) < 0){
+            alert("please draw a digit in order to test the network");
+            return;
+         }
+        var json = {
+            image: this.data,
+            predict: true
+        };
+        this.sendData(json);
+     },
+
+     receiveResponse: function(xmlHttp) {
+         if (xmlHttp.status != 200) {
+             alert("server returned status " + xmlHttp.status);
+             return;
+         }
+         var responseJSON = JSON.parse(xmlHttp.respenseText);
+         if (xmlHttp.responseText && responsejson.type == "test"){
+             alert("the neural network predicts you wrote a \'"
+                 + responseJSON.result + '\'');
+         }
+     }
+
+    onError: function(e) {
+        alert("Error occurred while connecting to server: " + e.target.statusText);
+    },
+
+    sendData: function(json){
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open('POST', this.HOST + ":" this.PORT, false);
+        xmlHttp.onload = function() { this.receiveResponse(xmlHttp);}.bind(this);
+        xmlHttp.onerror = function() {this.onError(xmlHttp)}.bind(this);
+        var msg =JSON.stringify(json);
+        xmlHttp.setRequestHeader('Content-length', msg.length);
+        xmlHttp.setRequestHeader("Connection", "close");
+        xmlHttp.send(msg);
+
+     }
+}
